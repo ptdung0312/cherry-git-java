e2f4e8f1d1507c6088d4995e1866f8286f5da9b6
DungPhanApricus <dung.phan@enovax.com>
2022-10-13 18:58:56 +0800

    change kubernate

--- src/main/java/org/example/list/rewrite/Kubernates.java
+++ src/main/java/org/example/list/rewrite/Kubernates.java
@@ -1,7 +1,17 @@
 package org.example.list.rewrite;
 
 public class Kubernates {
+	private String name;
+
 	public static void main(String[] args) {
 		System.out.println("Main - Kubernates");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
