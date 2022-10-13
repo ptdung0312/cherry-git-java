0c18d02295215e448b70eac027a08d814fb33714
DungPhanApricus <dung.phan@enovax.com>
2022-10-13 19:03:13 +0800

    change Exercise3

--- src/main/java/org/example/list/Exercise3.java
+++ src/main/java/org/example/list/Exercise3.java
@@ -7,9 +7,19 @@ import java.util.Optional;
  * Given the list of employees, find any the employee whose name is John.
  */
 public class Exercise3 {
+	private String name;
+
 	public static void main(String[] args) {
 		List<Employee> employees = StreamGetListOfEmployees.getListOfEmployees();
 		Optional<Employee> employee = employees.stream().filter(e -> "John".equalsIgnoreCase(e.getName())).findAny();
 		employee.ifPresent(System.out::println);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
