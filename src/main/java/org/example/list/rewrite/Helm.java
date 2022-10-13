641cc56ee62c311d05f974b79a5b2438a2e1f796
DungPhanApricus <dung.phan@enovax.com>
2022-10-13 18:52:33 +0800

    hot fix 3

--- src/main/java/org/example/list/rewrite/Helm.java
+++ src/main/java/org/example/list/rewrite/Helm.java
@@ -1,6 +1,16 @@
 package org.example.list.rewrite;
 
 public class Helm {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("main Helm");
 	}
