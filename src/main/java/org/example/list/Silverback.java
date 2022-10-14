e01b5e9208483f36d31b9e1a4ff47aeed6688f12
DungPhanApricus <dung.phan@enovax.com>
2022-10-14 12:54:30 +0800

    branch main commit to rebase 1

--- src/main/java/org/example/list/Silverback.java
+++ src/main/java/org/example/list/Silverback.java
@@ -8,4 +8,8 @@ public class Silverback {
 	private String name;
 
 	private String value;
+
+	public static void main(String[] args) {
+		System.out.println("branch main commit to rebase 1");
+	}
 }
