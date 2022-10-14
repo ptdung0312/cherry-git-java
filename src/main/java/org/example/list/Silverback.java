d58ac945f650287fbc78c7654ba9092d01334f9b
DungPhanApricus <dung.phan@enovax.com>
2022-10-14 13:02:13 +0800

    branch main commit to merge 1

--- src/main/java/org/example/list/Silverback.java
+++ src/main/java/org/example/list/Silverback.java
@@ -11,5 +11,6 @@ public class Silverback {
 
 	public static void main(String[] args) {
 		System.out.println("branch main commit to rebase 1");
+		System.out.println("branch main commit to merge 1");
 	}
 }
