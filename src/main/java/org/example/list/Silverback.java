65df854f89da7c9d22c24e661ab7d8a24ee93bdb
DungPhanApricus <dung.phan@enovax.com>
2022-10-14 16:28:37 +0800

    main commit to demo merge 1

--- src/main/java/org/example/list/Silverback.java
+++ src/main/java/org/example/list/Silverback.java
@@ -12,5 +12,6 @@ public class Silverback {
 	public static void main(String[] args) {
 		System.out.println("branch main commit to rebase 1");
 		System.out.println("branch main commit to merge 1");
+		System.out.println("branch main commit to demo merge 1");
 	}
 }
